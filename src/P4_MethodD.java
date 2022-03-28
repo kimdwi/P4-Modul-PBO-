74995dd83b5ba4a09449b578e22152fd34234eb8
kimdwi <parkdwiputriani@gmail.com>
2022-03-28 17:27:27 +0700

    motode dengan nilai kembali yang memiliki parameter

---
+++ src/P4_MethodD.java
@@ -0,0 +1,10 @@
+public class P4_MethodD {
+    public static void main(String[] args) {
+        double hasilBagi = bagi(9,3);
+        System.out.println("Hasil bagi = " + hasilBagi);
+    }
+
+    private static double bagi(double param1, double param2){
+        return (param1 / param2);
+    }
+}
