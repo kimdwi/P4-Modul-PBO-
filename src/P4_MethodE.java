6131df588a082026648f8c1bb86e3fc513476eee
kimdwi <parkdwiputriani@gmail.com>
2022-03-28 17:34:16 +0700

    array sebagai parameter metode

---
+++ src/P4_MethodE.java
@@ -0,0 +1,12 @@
+public class P4_MethodE {
+    public static void main(String[] args) {
+        int[] myArray = {1,3,5,7,9,11};
+        cetarArray(myArray);
+    }
+    private static void cetarArray(int[] array){
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(String.valueOf(array[i]) + " ");
+        }
+        System.out.println();
+    }
+}
