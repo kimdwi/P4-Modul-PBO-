2202fb2734d1564d3616798ce9f3e8b94ec1aff4
kimdwi <parkdwiputriani@gmail.com>
2022-03-28 16:52:40 +0700

    motode tanpa nilai kembali(void)

---
+++ src/MethodA.java
@@ -0,0 +1,9 @@
+public class MethodA {
+    public static void main(String[] args) {
+        welcome();
+    }
+
+    private static void welcome() {
+        System.out.println("Selamat belajar java");
+    }
+}
