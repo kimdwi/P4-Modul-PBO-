ad0b2f73a9d605b912b0f69c0f0641d000c81fd4
kimdwi <parkdwiputriani@gmail.com>
2022-03-28 18:45:01 +0700

    metode construktor

--- src/P4_Orang.java
+++ src/P4_Orang.java
@@ -9,31 +9,22 @@ public class P4_Orang {
     public P4_Orang() {
     }
 
-    public P4_Orang(String nama) {
+    public P4_Orang(String nama, int umur) {
         this.nama = nama;
+        this.umur = umur;
     }
 
     public void menangis() {
+
         System.out.println("ue..ue..TvT");
     }
 
     public void tertawa() {
+
         System.out.println("www...www");
     }
 
-    public static void main(String[] args) {
-        P4_Orang dido = new P4_Orang();
-        dido.nama = "Dido";
-        dido.umur = 4;
-        System.out.println("Nama : " + dido.nama);
-        System.out.println("Umur : " + dido.umur + "tahun.");
-        dido.menangis();
 
-        System.out.println();
-
-        P4_Orang dodo = new P4_Orang("Dodo");
-        System.out.println("Nama : " + dodo.nama);
-        System.out.println("Umur : " + dodo.umur + "tahun.");
-        dodo.tertawa();
     }
+
 }
--- src/P4_TestOrang.java
+++ src/P4_TestOrang.java
@@ -1,18 +1,12 @@
 public class P4_TestOrang {
     public static void main(String[] args) {
-        P4_Orang dido = new P4_Orang();
-        dido.nama = "Dido";
-        dido.umur = 4;
-        System.out.println("Nama : " + dido.nama);
-        System.out.println("Umur : " + dido.umur + "tahun.");
-        dido.menangis();
+        P4_Orang badu = new P4_Orang();
+        badu.nama = "Dido";
+        badu.umur = 12;
+        System.out.println("Nama : " + badu.nama);
+        System.out.println("Umur : " + badu.umur + "tahun.");
+        badu.tertawa();
 
-        System.out.println();
-
-        P4_Orang dodo = new P4_Orang("Dodo");
-        System.out.println("Nama : " + dodo.nama);
-        System.out.println("Umur : " + dodo.umur + "tahun.");
-        dodo.tertawa();
 
     }
 }
